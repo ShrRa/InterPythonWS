508245e9c0f6e59b3c0a31281422d9a9f031546b
ShrRa <shr.razim@gmail.com>
2024-12-09 18:53:24 +0100

    Parametrized tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -27,6 +27,50 @@ def test_max_mag_zeros():
 
     assert max_mag(test_input_df, test_input_colname) == test_output
 
+
+
+def test_max_mag_all():
+    #Test max_mag function against multiple cases
+    from lcanalyzer.models import max_mag
+
+    cases = [{'test_input_df':pd.DataFrame(data=[[0, 0, 0], 
+                                       [0, 0, 0], 
+                                       [0, 0, 0]], columns=list("abc")),
+             'test_input_colname':'b',
+             'test_output':0},
+             {'test_input_df':pd.DataFrame(data=[[1, 5, 3], 
+                                       [7, 8, 9], 
+                                       [3, 4, 1]], columns=list("abc")),
+             'test_input_colname':'a',
+             'test_output':7}]
+    
+    for case in cases:
+        assert max_mag(case['test_input_df'], case['test_input_colname']) == case['test_output']
+
+"""
+@pytest.mark.parametrize(
+    "test_df, test_colname, expected",
+    [
+        (pd.DataFrame(data=[[1, 5, 3], 
+                            [7, 8, 9], 
+                            [3, 4, 1]], 
+                      columns=list("abc")),
+        "a",
+        17),
+        (pd.DataFrame(data=[[0, 0, 0], 
+                            [0, 0, 0], 
+                            [0, 0, 0]], 
+                      columns=list("abc")),
+        "b",
+        0),
+    ])
+def test_max_mag(test_df, test_colname, expected):
+    #Test max function works for array of zeroes and positive integers.
+    from lcanalyzer.models import max_mag
+    assert max_mag(test_df, test_colname) == expected
+"""
+    
+
 def test_min_mag_negatives():
    # Test that min_mag function works for negatives
    from lcanalyzer.models import min_mag
